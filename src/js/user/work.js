a629e4ba4b3d2c6fc58d3732bc3d631d04e498f7
kevindoveton <kevindoveton@me.com>
2017-01-19 13:19:42 +0200

    modified work js a bit more

--- src/js/user/work.js
+++ src/js/user/work.js
@@ -1,11 +1,17 @@
 function closeWorkModal() {
-	$(".workInfo").css({display: 'none'});
+	$(".workInfo").css({opacity: 0,'z-index': -1});
 }
 
 function openWorkModal(t) {
-	console.log(t)
+	$(".workInfo").css({'z-index': 3});
 	// t.children().closest("span.modalContent")
 	var content = $(t).children().closest("span.modalContent").html()
 	$("#modalContent").html(content)
-	$(".workInfo").css({display: 'block'});
+	$(".workInfo").css({opacity: 1});
 }
+
+$(function() {
+	$(".workInfo").on({
+		'click' : closeWorkModal
+	});
+});
