882ff152a6fd49234b97451e89a0630ad649d4fd
kevindoveton <kevindoveton@me.com>
2017-01-25 11:26:42 +1030

    contact form completed (not tested)

--- src/contact.php
+++ src/contact.php
@@ -1,20 +1,43 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+$to = "kevindoveton@me.com"
 
-$msg = 	"<h1>Contact Form:</h1>";
-$msg +=	"<h1>Name</h1><p>";
-$msg +=	$name;
-$msg +=	"</p><h2>Email</h2><p>";
-$msg +=	$email;
-$msg +=	"</p><h2>Message</h2><p>";
-$msg +=	$message;
-$msg +=	"</p>";
+function reportError() {
+	echo "something went wrong, please try again.";
+}
 
-// mail($msg);
+if ($_POST) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
 
-echo "failed";
+	$msg = 	"<h1>Contact Form:</h1>";
+	$msg +=	"<h1>Name</h1><p>";
+	$msg +=	$name;
+	$msg +=	"</p><h2>Email</h2><p>";
+	$msg +=	$email;
+	$msg +=	"</p><h2>Message</h2><p>";
+	$msg +=	$message;
+	$msg +=	"</p>";
 
+	// Email Headers
+	$headers = "From: " . $email . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+	// send the email
+	$mail = mail($to, 'kdoveton.com', $msg, $headers, '-f'.$to);
+
+	// check if sent
+	if($mail) {
+		echo 'success';
+	}
+	else {
+		reportError();
+	}
+}
+else {
+	reportError();
+}
 ?>
